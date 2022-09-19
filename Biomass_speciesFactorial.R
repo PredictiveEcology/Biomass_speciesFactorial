56820d5f1ba21923f53c56fc741fa0539e0a6ef1
CeresBarros <cbarros@mail.ubc.ca>
2022-09-19 11:25:36 -0700

    check `Biomass_core` existence/version and install if necessary
    Code brought from `B_borealDataPrep`, to move to a function eventually, see https://github.com/PredictiveEcology/Biomass_speciesFactorial/issues/1

--- Biomass_speciesFactorial.R
+++ Biomass_speciesFactorial.R
@@ -200,9 +200,31 @@ RunExperiment <- function(speciesTableFactorial, maxBInFactorial, knownDigest, f
   sppColors <- viridis::viridis(n = NROW(speciesTableFactorial))
   names(sppColors) <-  speciesTableFactorial$species
 
-  moduleNameAndBranch <- c("Biomass_core@development (>= 1.3.9)")
-  modules <- gsub("@.+", "", moduleNameAndBranch)
-  getModule(moduleNameAndBranch, modulePath = paths$modulePath, overwrite = TRUE) # will only overwrite if wrong version
+  ## TODO: make the following into a function to use across modules (e.g. B_borealDataPrep)
+  paths <- paths(sim)
+  curModPath <- file.path(paths$modulePath, currentModule(sim))
+  submodule <- "Biomass_coreSubModule"
+  paths$outputPath <- file.path(curModPath, submodule, "outputs", rndstr()) ## avoid race conditions
+  on.exit(unlink(paths$outputPath, recursive = TRUE), add = TRUE)
+
+  modules <- modules(sim)
+  if (!any(modules == "Biomass_core") ||
+      moduleVersion("Biomass_core", paths$modulePath) < "1.3.5") { # if Biomass_core doesn't exist in modulePath or is too old, then download it
+    ## check that SpaDES.install is available in the right version
+    if (!Require::Require("PredictiveEcology/SpaDES.project (>= 0.0.7)",
+                          upgrade = FALSE, install = FALSE)) {
+      stop(paste("Please install SpaDES.project v0.0.7 or above using:",
+                 "Require('PredictiveEcology/SpaDES.project (>= 0.0.7)', require = FALSE)"))
+    }
+
+    paths$modulePath <- file.path(curModPath, submodule, "module")
+    moduleNameAndBranch <- c("Biomass_core@development (>= 1.3.9)")
+    modules <- list(gsub("@.+", "", moduleNameAndBranch))
+    SpaDES.project::getModule(moduleNameAndBranch, modulePath = paths$modulePath, overwrite = TRUE) # will only overwrite if wrong version
+  } else {
+    ## trim unnecessary modules:
+    modules <- modules[modules == "Biomass_core"]
+  }
 
   parameters <- list(
     Biomass_core = list(
